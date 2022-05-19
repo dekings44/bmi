37937788f3fdee99d0caa0da29ba1dca084abaeb
Kingsley Ihemere <dekings44@gmail.com>
2022-05-19 13:41:59 +0100

    My bmi script first commit

---
+++ bmi.py
@@ -0,0 +1,8 @@
+
+
+height = float(input('What is your height in m?\n'))
+weight = float(input('What is your weight in kg?\n'))
+
+bmi = weight / height ** 2
+
+print('Your body mass index is {}'.format(bmi))
