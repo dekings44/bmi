f7bf9ecafa34530c9564afd0fdd772a8c90512fb
Kingsley Ihemere <dekings44@gmail.com>
2022-05-19 13:43:26 +0100

    update the output to int

--- bmi.py
+++ bmi.py
@@ -3,6 +3,6 @@
 height = float(input('What is your height in m?\n'))
 weight = float(input('What is your weight in kg?\n'))
 
-bmi = weight / height ** 2
+bmi = int(weight / height ** 2)
 
 print('Your body mass index is {}'.format(bmi))
