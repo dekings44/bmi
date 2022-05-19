39d15bb27cfbf3ec89acba9a12540e892df911d0
Kingsley Ihemere <dekings44@gmail.com>
2022-05-19 20:41:44 +0100

    checking for obesity

--- bmi.py
+++ bmi.py
@@ -5,4 +5,13 @@ weight = float(input('What is your weight in kg?\n'))
 
 bmi = round(weight / height ** 2, 2)
 
-print('Your body mass index is {}'.format(bmi))
+if bmi < 18.5:
+    print(f'Your body mass index is {bmi} and you are underweight')
+elif bmi < 25:
+    print(f'Your body mass index is {bmi} and you have a normal weight')
+elif bmi < 30:
+    print(f'Your body mass index is {bmi} and you are overweight')
+elif bmi < 35:
+    print(f'Your body mass index is {bmi} and you are obese')
+else:
+    print(f'Your body mass index is {bmi} and you are clinically obese')
